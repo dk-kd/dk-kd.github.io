5d2457d40bc4befca9dfd6fb44e47d0caa67bb04
dk-kd <82509916+dk-kd@users.noreply.github.com>
2021-04-25 17:05:25 +0900

    Fix audio func

--- m/js/audio.js
+++ m/js/audio.js
@@ -1,19 +1,13 @@
 let audio = null;
 const audio_pass = "./audio/";
-const audio_list = {
-    "kohane": ["kohane_1.mp3", "kohane_2.mp3"],
-    "an": ["an_1.mp3", "an_2.mp3"]
-};
 const playAudio = (name) => {
     if (audio != null) {
         audio.pause();
     }
-    if (typeof audio_list[name] == "undefined") {
-        console.log("undefined audio: " + name);
-        return;
+    try {
+        audio = new Audio(audio_pass + name);
+        audio.play();
+    } catch (e) {
+        console.log(e);
     }
-    const arr = audio_list[name];
-    const mp3 = arr[Math.floor(Math.random() * arr.length)];
-    audio = new Audio(audio_pass + mp3);
-    audio.play();
 }
